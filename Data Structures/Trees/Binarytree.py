c042ffad542327bf840adaf774f6759d2d3b682c
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-02-01 01:00:06 +0200

    't'

--- Data Structures/Trees/Binarytree.py
+++ Data Structures/Trees/Binarytree.py
@@ -1,50 +1,57 @@
-class Tree:
-	'a binary tree'
-	class Node:
-		def __init__(self,data):
-			self.left = None
-			self.right = None
-			self.data = data
-		# do i need to make this an instance method, should it be a class or static method ?
-		def insert(self,data):
-			if data < self.data:
-				if self.left != None:
-					self.insert(data)
-				else:
-					self.left = Tree.Node(data)
-			else:
-				if self.right != None:
-					self.insert(data) 
-				else:
-					self.left = Tree.Node(data)
-		
-		def contain(self,data):
-			if data < self.data:
-				if self.left != None:
-					self.contain(data) 
-				else:
-					return False
-			elif data > self.data:
-				if self.right != None:
-					self.contain(data)
-				else:
-					return False
-			else:
-				return True
-		def printpostorder()
-			
+
+class Node:
 	def __init__(self,data):
-		self.root = self.Node(data)
+		self.left = None
+		self.right = None
+		self.data = data
+	# do i need to make this an instance method, should it be a class or static method ?
+	def insert(self,data):
+		if data < self.data:
+			if self.left != None:
+				self.insert(data)
+			else:
+				self.left = Node(data)
+		else:
+			if self.right != None:
+				self.insert(data) 
+			else:
+				self.left = Node(data)
 	
-	def insert(self, data):
-		self.root.insert(data)
-	
-	def contain(self, data):
-		self.root.contain(data)
+	def contain(self,data):
+		if data < self.data:
+			if self.left != None:
+				self.contain(data) 
+			else:
+				return False
+		elif data > self.data:
+			if self.right != None:
+				self.contain(data)
+			else:
+				return False
+		else:
+			return True
+	def print_preorder(self):
+		print_preorder(self.left)
+		print(self.data,end="")
+		print_preorder(self.right)
+		
+	def printpostorder(self):
+		pass
+		
 
 
-t = Tree(5)
-t.insert(9)    
+
+t = Node(5)
+t.insert(1)
+t.insert(3)    
+t.insert(10)    
+t.insert(4)    
+t.insert(2)    
+t.insert(7)    
+t.insert(13)
+
+    
+
 	
 
 	
