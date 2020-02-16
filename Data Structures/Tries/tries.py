77ef6c9bb527776c046369a38c3820f24c480eee
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-02-16 16:18:16 +0200

    'm'

--- Data Structures/Tries/tries.py
+++ Data Structures/Tries/tries.py
@@ -1,3 +1,12 @@
-class Try:
-    # try
-    pass 
+# class Tri:
+#     'tri'
+#     class Node:
+#         def __init__(self, valid):
+#             self.letterset = {}
+#             self.boolean : bool
+        
+#         def insert(self,word):
+            
+
+            
+#     pass 
